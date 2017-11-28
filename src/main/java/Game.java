d8ed8915782f28ef3e734433f4d2bd0cf53edd18
ScreaMonhik <dimonhik5002@mail.ru>
2017-11-28 22:53:07 +0200

    Part 5

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -67,20 +67,20 @@ public class Game implements Runnable, MouseListener {
 
     public void BoxTrigger(double x, double y) {
         if (x >= 60.0 && y >= 60.0 && x <= 852.0 && y <= 157.0) {
-            if (x >= 156.0 && x <= 255.0) System.out.println("sdfjDAOGFHDSO");
-            if (x >= 354.0 && x <= 453.0) System.out.println("KLETKA 2");
-            if (x >= 552.0 && x <= 651.0) System.out.println("KLETKA 3");
-            if (x >= 750.0 && x <= 849.0) System.out.println("KLETKA 4");
+            if (x >= 156.0 && x <= 255.0) checkCell(156,60);
+            if (x >= 354.0 && x <= 453.0) checkCell(354,60);
+            if (x >= 552.0 && x <= 651.0) checkCell(750,60);
+            if (x >= 750.0 && x <= 849.0) checkCell(750,60);
         }
         if (x >= 60.0 && y >= 159.0 && x <= 852.0 && y <= 256.0) {
-            if (x >= 56.0 && x <= 155.0) System.out.println("1 KLETKA 2 R");
-            if (x >= 254.0 && x <= 353.0) System.out.println("2 kletka 2 r");
-            if (x >= 452.0 && x <= 551.0) System.out.println("3 kletka 2 r");
-            if (x >= 650.0 && x <= 749.0) System.out.println("4 kletka 2r");
+            if (x >= 56.0 && x <= 155.0)  checkCell(56,159);
+            if (x >= 254.0 && x <= 353.0) checkCell(254,159);
+            if (x >= 452.0 && x <= 551.0) checkCell(452,159);
+            if (x >= 650.0 && x <= 749.0) checkCell(650,159);
         }
         if (x >= 60.0 && y >= 258.0 && x <= 852.0 && y <= 355.0) {
-            if (x >= 156.0 && x <= 255.0) System.out.println("sdfjDAOGFHDSO");
-            if (x >= 354.0 && x <= 453.0) System.out.println("KLETKA 2");
+            if (x >= 156.0 && x <= 255.0) checkCell(156,258);
+            if (x >= 354.0 && x <= 453.0) checkCell(354,258);
             if (x >= 552.0 && x <= 651.0) checkCell(552,258);
             if (x >= 750.0 && x <= 849.0) checkCell(750,258);
         }
